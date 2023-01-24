7caeadc8311fe8e23cb3eb9c367238d8c727a82e
Luke David Kozlowski <kozlow86@ganymede.egr.msu.edu>
2023-01-24 11:48:22 -0500

    I plotted the water level over the years for Lake Ontario

--- Great_Lakes_Water_Levels.py
+++ Great_Lakes_Water_Levels.py
@@ -1,4 +1,4 @@
-""""
+"""
 To use this notebook for your in-class assignment, you will need these 
 files, which you shoujld have downloaded:
 * mhu.csv -- Lake Michigan and Lake Huron
@@ -10,7 +10,7 @@ As instructed in the in-class activity notebook for today, you are
 only expected to complete one PART below. Do not worry if your group 
 is not big enough to finish all parts below, but if you have extra 
 time, you're welcome to do so.
-""""
+"""
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -38,7 +38,12 @@ import matplotlib.pyplot as plt
 # Using the Ontario Dataset, plot the Water Level, the second column, 
 # as a function of time years
 
+# +
+ont_file = pd.read_csv("ont.csv", skiprows = 0)
 
+plt.plot(ont_file["year"], ont_file["Lake Ontario annual averages"])
+plt.savefig("ont.png")
+# -
 
 # PART 5
 # Using the Michigan/Huron and Superior Datasets, plot the 
@@ -55,7 +60,7 @@ import matplotlib.pyplot as plt
 
 
 # PART 7
-#Using the Superior and Ontario Datasets, plot the Superior Water 
+# Using the Superior and Ontario Datasets, plot the Superior Water 
 # Level vs Ontario Water Level to see if there is any correlation 
 # between the water levels.
 
